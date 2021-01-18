dc226713c8bdc581a3f85a9aa9e7ef19c0178379
Rodrigo Dinis <rodrigodinis20@gmail.com>
2021-01-18 18:43:48 -0100

    file Hello.java

---
+++ module-1/Hello.java
@@ -0,0 +1,10 @@
+class Hello{
+
+	public static void main(String[] banana){
+
+		if(banana.length >= 1){
+			System.out.println("Welcome " + banana[0] + "\n");
+		}
+		System.out.println("Hello World - whatsup SOUTaqueOVERFLOW?!?");
+	}
+}
