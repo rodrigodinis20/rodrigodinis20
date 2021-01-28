0f475122ed79184319fcfd4e6829dae16b5531e4
benny <bernardo.fernandes@academiadecodigo.org>
2021-01-28 10:57:10 -0100

    add live coding enum example

---
+++ live-coding/enum-live-coding/src/org/academiadecodigo/bootcamp/enums/Sandbox.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.bootcamp.enums;
+
+public class Sandbox {
+
+    public static void main(String[] args){
+
+        DrinkType currentDrink = DrinkType.BEER;
+
+        currentDrink.drink();
+    }
+}
